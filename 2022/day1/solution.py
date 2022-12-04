be6f3e6a8bbbf1a4a72c71c80dbbfee286cd3865
Dignani Alessandro <alessandro.dignani@leitha.eu>
2022-12-04 21:08:59 +0100

    add solution day 1 part 2

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -1,15 +1,18 @@
 def main():
-    best = 0
+    third = 0
+    second = 0
+    first = 0
     current = 0
     with open('input.txt') as f:
         for num in f.readlines():
             if num == '\n':
-                if current > best:
-                    best = current
+                if current > third:
+                    third = current
+                    first, second, third = sorted((first, second, third), reverse=True)
                 current = 0
             else:
                 current += int(num)
-    print(best)
+    print(first + second + third)
 
 
 if __name__ == '__main__':
